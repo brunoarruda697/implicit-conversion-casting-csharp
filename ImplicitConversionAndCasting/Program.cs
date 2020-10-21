65a632e73ae74c12f63c843c3750051f59df5d9d
Bruno Costa <brunoarruda697@gmail.com>
2020-10-21 11:00:24 -0300

    Conversão implicita e casting

---
+++ ImplicitConversionAndCasting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImplicitConversionAndCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            float b;
+            int c;
+
+            a = 5.1;
+            b = (float)a;
+            
+            // *double tem um tamanho maior que o float em bytes, por isso caso queira fazer essa conversão
+            // precisa fazer a conversão implicita com o casting -> (float)
+            Console.WriteLine(b);
+
+            c = (int)a;
+            Console.WriteLine(c);
+
+            int x = 5;
+            int y = 2;
+            double result = x / y;
+            Console.WriteLine(result);
+            result = (double)x / y;
+            Console.WriteLine(result);
+        }
+    }
+}
